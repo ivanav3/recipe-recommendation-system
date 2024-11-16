efbcb56ef30de894e374fc52396665b0581e1277
Ivana Vukmirov <ivanav32001@gmail.com>
2024-11-16 19:16:37 +0100

    Add to favorites

--- src/recipe_recommendation_system/core.clj
+++ src/recipe_recommendation_system/core.clj
@@ -5,8 +5,6 @@
 
 (def filename "first-cleaned5.csv")
 
-(slurp filename)
-
 (defn parse
   "Convert a CSV string into rows of columns, removing unwanted characters."
   [string]
@@ -17,7 +15,6 @@
              (when (not (str/blank? trimmed-row))
                (str/split trimmed-row #";"))))
          (str/split cleaned-string #"\n"))))
-(rest (parse (slurp filename)))
 
 (def initial-dataset (agent (rest (parse (slurp filename)))))
 
@@ -54,8 +51,8 @@
         (println "Password:")
         (let [password (read-line)]
           (send registered-users
-                (fn [korisnici]
-                  (conj korisnici {:username username :password (hash-password password)})))
+                (fn [users]
+                  (conj users {:username username :password (hash-password password) :favs []})))
           (await registered-users)
           (println "Registered!" username))))))
 
@@ -68,16 +65,98 @@
   (println "Username:" (:username u) ", Password:" (:password u)))
 
 
+(def logged-in-users (agent []))
+
+(defn is-user-logged-in? [username]
+  (some #(= username (:username %)) @logged-in-users))
+
+(defn remove-user [users username]
+  (filter #(not= username (:username %)) users))
+
+(defn logout []
+  (println "Enter username to logout:")
+  (let [username (read-line)]
+    (if (is-user-logged-in? username)
+      (do
+        (send logged-in-users remove-user username)
+        (println username "has been logged out."))
+      (println "No such user is logged in."))))
+
+
+(defn find-by-title [title]
+  (let [lower-title (str/lower-case title)]
+    (filter (fn [item]
+              (let [title (:title item)]
+                (str/includes? (str/lower-case title) lower-title))) @initial-dataset)))
+
+(defn update-favs [users username chosen-recipe]
+  (map #(if (= username (:username %))
+          (update % :favs conj chosen-recipe)
+          %)
+       users))
+
+(defn add-to-favs [favs chosen-recipe]
+  (if (not-any? #(= % chosen-recipe) favs)
+    (conj favs chosen-recipe)
+    (println "Already marked as fav.")))
+
+(defn update-favs [users username chosen-recipe]
+  (map #(if (= username (:username %))
+          (update % :favs add-to-favs chosen-recipe)
+          %)
+       users))
+
+
+(defn choose-fav [username]
+  (println "Enter recipe title or part of title:")
+  (let [title (read-line)
+        results (find-by-title title)]
+    (if (seq results)
+      (do
+        (println "Found the following recipes:")
+        (doseq [result results]
+          (println (:title result)))
+
+        (println "Please enter the full title of the recipe you're interested in:")
+        (let [chosen-title (str/lower-case (read-line))
+              chosen-recipe (some #(if (= (str/lower-case (:title %)) chosen-title) %) results)]
+          (if chosen-recipe
+            (send registered-users update-favs username chosen-recipe)
+            (println "Error. Try again."))))
+      (println "No recipes found."))))
+
+(defn main-menu [username]
+  (println "\nMain Menu:")
+  (println "1. Choose a recipe")
+  (println "2. Logout")
+  (println "Please select an option:")
+
+  (let [option (read-line)]
+    (cond
+      (= option "1") (choose-fav username)
+      (= option "2") (logout)
+      :else (do
+              (println "Invalid option. Please try again.")
+              (main-menu username)))))
+
 (defn login []
   (println "Username:")
   (let [username (read-line)]
     (println "Password:")
     (let [password (read-line)]
-      (let [korisnik (some #(when (and (= username (:username %))
-                                       (= (hash-password password) (:password %))) %)
-                           @registered-users)]
-        (if korisnik
-          (println "Welcome, " username)
-          (println "Error. Try again."))))))
+      (if (some #(= username (:username %)) @logged-in-users)
+        (println "User is already logged in. Please logout first.")
+        (let [u (some #(when (and (= username (:username %))
+                                  (= (hash-password password) (:password %))) %)
+                      @registered-users)]
+          (if u
+            (do
+              (println "Welcome, " username)
+              (send logged-in-users conj {:username username})
+              (main-menu username))
+            (println "Error. Try again.")))))))
 
 (login)
+(logout)
+
+@registered-users
--- test/recipe_recommendation_system/core_test.clj
+++ test/recipe_recommendation_system/core_test.clj
@@ -1,6 +1,8 @@
 (ns recipe-recommendation-system.core-test
   (:require [clojure.test :refer :all]
-            [recipe-recommendation-system.core :refer :all]))
+            [recipe-recommendation-system.core :refer :all])
+;;            [midje.sweet :refer :all])
+  )
 
 (deftest a-test
   (testing "FIXME, I fail."
