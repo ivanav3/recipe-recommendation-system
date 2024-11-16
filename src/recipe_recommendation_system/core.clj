daa586fc7014f08265b992b0c3a46cfb07691af3
Ivana Vukmirov <ivanav32001@gmail.com>
2024-11-16 12:57:06 +0100

    Updated hash function

--- src/recipe_recommendation_system/core.clj
+++ src/recipe_recommendation_system/core.clj
@@ -3,7 +3,7 @@
   (:require [clojure.string :as str])
   (:import (java.security MessageDigest)))
 
-(def filename "first-cleaned.csv")
+(def filename "first-cleaned5.csv")
 
 (slurp filename)
 
@@ -19,29 +19,33 @@
          (str/split cleaned-string #"\n"))))
 (rest (parse (slurp filename)))
 
-(def initial-dataset (rest (parse (slurp filename))))
+(def initial-dataset (agent (rest (parse (slurp filename)))))
 
-(def keys [:title :total-time :serving-size :ingr :instructions])
+(def keys (agent [:title :total-time :serving-size :ingr :instructions]))
 
 (defn vectors-to-maps [vectors]
-  (map #(zipmap keys %) vectors))
+  (map #(zipmap @keys %) vectors))
 
-(def map-dataset (vectors-to-maps initial-dataset))
-(def initial-dataset (doall map-dataset))
+(send initial-dataset vectors-to-maps)
+
+(defn clean-ingr [recipes]
+  (map #(update % :ingr (fn [ingr] (str/split ingr #",\s*"))) recipes))
+
+(send initial-dataset clean-ingr)
 
 (defn hash-password [password]
   (let [md (MessageDigest/getInstance "SHA-256") ;; Object of class MessageDigest configured for using SHA-256 algorithm
-        hashed-bytes (.digest md (.getBytes password))] ;;getBytes as a method implemented in String class is used for creating this into bytes array
+        hashed-bytes (.digest md (.getBytes password))] ;;getBytes as a method implemented in String class is used for changing this into bytes array
         ;;parameters for .digest are algorithm and data
     (apply str (map #(format "%02x" %) hashed-bytes))))
     ;;this is only for converting into hex format
 
-(defn create-user [username password]
-  (let [hashed-password (hash-password password)]
-    {:username username
-     :password hashed-password}))
 
-(let [username "ivana"
-      password "sifra12345"
-      user (create-user username password)]
-  (println "Created user:" user))
+
+(def user-a (agent {:username "ivana" :password "sifra12345"}))
+
+(send user-a
+      #(assoc % :password (hash-password (:password %))))
+
+(@user-a :password)
+(hash-password "sifra12345")
